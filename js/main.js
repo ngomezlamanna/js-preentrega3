a11edea0772e7aa428dfb5279ae8f97a6b2cf0b9
ngomezlamanna <ngomezlamanna@gmail.com>
2024-03-17 23:58:52 -0300

    entrega numero3

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 //js para index.html (inicio de sesion)
 
 const formInicio = document.querySelector("#formIni"),
-usuarioInicio = document.querySelector("#usuarioIni"),
-contraseniaInicio = document.querySelector("#contraseniaIni"),
-pFalso = document.querySelector("#usuarioFalso");
+    usuarioInicio = document.querySelector("#usuarioIni"),
+    contraseniaInicio = document.querySelector("#contraseniaIni"),
+    pFalso = document.querySelector("#usuarioFalso");
 
-function ingresoSesion (usuarios) {
+function ingresoSesion(usuarios) {
     let usuarioEncontrado = usuarios.find((usuario) => {
         return usuario.usuario == usuarioInicio.value && usuario.contrasenia == contraseniaInicio.value;
     })
 
     //OPERADOR "?"
-    usuarioEncontrado ? location.href= "../html/productos.html" : document.querySelector('#usuarioFalso').innerHTML = "<strong style='color: red'>Usuario no registrado. ¡Regístrese!</strong>";
+    usuarioEncontrado ? location.href = "../html/productos.html" : document.querySelector('#usuarioFalso').innerHTML = "<strong style='color: red'>Usuario no registrado. ¡Regístrese!</strong>";
 }
 
-function recuperarLs () {
-    return JSON.parse (localStorage.getItem("usuarios"));
+function recuperarLs() {
+    return JSON.parse(localStorage.getItem("usuarios"));
 }
 
 const usuarioLs = recuperarLs();
